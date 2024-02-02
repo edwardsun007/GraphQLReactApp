4c98f05af72d020515eecb5f23ebefd6fcf6df77
edward sun <edwardsun007@gmail.com>
2024-02-02 15:05:32 -0800

    hard code our test Data and creating basic graphql query using lodash

--- schema/schema.js
+++ schema/schema.js
@@ -3,8 +3,10 @@
  const {
     GraphQLObjectType,
     GraphQLInt,
-    GraphQLString
+    GraphQLString,
+    GraphQLSchema
  } = graphql; // this is just destructing methods from graphql lib
+ const _ = require('lodash');
 
  // userType is object that instructs graphql what kind of user object should be
  const UserType = new GraphQLObjectType({
@@ -15,6 +17,12 @@
         age: {type: GraphQLInt},
     }
  });
+
+ // hard code data for now
+ const users = [
+    { id: '23', firstName: 'Bill', age: 20},
+    { id: '47', firstName: 'Samantha', age: 21}
+ ]
 
  const RootQuery = new GraphQLObjectType({
     // translate: you can give root query asking about users, 
@@ -29,8 +37,15 @@
             // the resolve function will actually go into the Data Store / Database, 
             // and it finds the data
             resolve(parentValue, args){ // parentValue is something that is not going to be used
-                // TODO
+              return _.find(users, {id: args.id}) // lodash search all users and return the user with id which is the passed id
             }
         }
     }
- })
+ });
+
+/**
+ * create a new graphQLschema by calling new, and then export it as common js MODULE so that other piece of the app can access it
+ */ 
+module.exports = new GraphQLSchema({
+    query: RootQuery
+});
