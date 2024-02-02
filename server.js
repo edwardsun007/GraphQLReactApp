61d80fdae05b01980f48cd9714dc01c0a10d25b0
edward sun <edwardsun007@gmail.com>
2024-02-02 00:23:54 -0800

    import and hook up express with graphQL

--- server.js
+++ server.js
@@ -2,9 +2,15 @@
  * mini Express backend server
  */
 const express = require('express');
+const expressGraphQL = require('express-graphql').graphqlHTTP; // glue layer between express and graphql
 
 const app = express();
 
+// app.use is the way Express to add middlewares
+app.use('/graphql', expressGraphQL({
+  graphiql: true
+}));
+
 app.listen(4000, () => {
-  console.log('Listening');
+  console.log('Listening on port 4000');
 });
